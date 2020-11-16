ddfff6dd4e42ef0820843c768d2407f9b2734441
Bruno Padilha <brpadilha.dev@gmail.com>
2020-11-16 15:23:15 -0300

    faz componentes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,67 @@
-import React, { useState, useEffect } from "react";
-import Product from "./Product";
+import React, { useState } from "react";
 
-const App = () => {
-  const [produto, setProduto] = useState(null);
+// import { Container } from './styles';
+import Input from "./components/Input";
+import Select from "./components/Select";
+import Radio from "./components/Radio";
+import Checkbox from "./components/Checkbox";
 
-  useEffect(() => {
-    const produtoLocal = localStorage.getItem("produto");
-
-    if (produtoLocal) setProduto(produtoLocal);
-  }, [produto]);
-
-  function handleClick({ target }) {
-    localStorage.setItem("produto", target.innerText);
-
-    setProduto(target.innerText);
-  }
+function App() {
+  const [nome, setNome] = useState("");
+  const [senha, setSenha] = useState("");
+  const [email, setEmail] = useState("");
+  const [produto, setProduto] = useState("");
+  const [cor, setCor] = useState("");
+  const [fruta, setFruta] = React.useState([]);
+  const [termos, setTermos] = React.useState([]);
 
   return (
     <>
-      <h1>Preferência: {produto}</h1>
-      <button style={{ margin: "1px" }} onClick={handleClick}>
-        smartphone
-      </button>
-      <button style={{ margin: "1px" }} onClick={handleClick}>
-        notebook
-      </button>
-      <button style={{ margin: "1px" }} onClick={handleClick}>
-        tablet
-      </button>
+      <form action="">
+        <h2>Frutas</h2>
+        <Checkbox
+          options={["Uva", "Laranja", "Limão"]}
+          value={fruta}
+          setValue={setFruta}
+        />
+        <Checkbox
+          options={["Termos e Condições"]}
+          value={termos}
+          setValue={setTermos}
+        />
+        <h2>Cores</h2>
+        <Radio
+          options={["azul", "verde", "amarelo"]}
+          value={cor}
+          setValue={setCor}
+        />
 
-      <Product produto={produto} />
+        <Select
+          options={["Notebook", "Smartphone", "Tablet"]}
+          value={produto}
+          setValue={setProduto}
+        />
+
+        <Input id="nome" label="Nome" value={nome} setValue={setNome} />
+        <Input
+          id="email"
+          label="Email"
+          value={email}
+          setValue={setEmail}
+          type="email"
+          required
+        />
+        <Input
+          id="nome"
+          label="Senha"
+          type="password"
+          value={senha}
+          setValue={setSenha}
+        />
+        <button>Enviar</button>
+      </form>
     </>
   );
-};
+}
+
 export default App;
---
+++ src/components/Select.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Select({ options, value, setValue, ...props }) {
+  return (
+    <select
+      value={value}
+      onChange={({ target }) => setValue(target.value)}
+      {...props}
+    >
+      <option value="" disabled>
+        Selecione
+      </option>
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
